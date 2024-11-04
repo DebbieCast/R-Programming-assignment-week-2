c19c21bf466557c836f7fc38693802bac61031ff
Deborah Castillo <deborahcastillo95@gmail.com>
2024-11-04 16:33:31 -0600

    Correcting mistake

--- complete.R
+++ complete.R
@@ -5,7 +5,7 @@ complete <- function(directory, id = 1:332){
     
     for (i in id) {
         data1 <- read.csv(lista[i]) ##Reading the files according to i
-        nobs <- sum(complete.cases(data1[i])) ##adding the # of observations
+        nobs <- sum(complete.cases(data1)) ##adding the # of observations
         DF <- rbind(DF, data.frame(i, nobs)) ## Creating the dataframe with 2 columns "i" and "nobs"
     }
     colnames(DF) <- c("id", "nobs")
